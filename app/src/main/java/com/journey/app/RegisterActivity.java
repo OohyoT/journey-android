ca54ff193ee22a3715c1e12a5fb4ff0c8399a266
Beta Kuang <beta.kuang@gmail.com>
2017-10-28 17:25:33 +0800

    add network connection

--- app/src/main/java/com/journey/app/RegisterActivity.java
+++ app/src/main/java/com/journey/app/RegisterActivity.java
@@ -6,6 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -38,7 +48,42 @@ public class RegisterActivity extends AppCompatActivity {
         String phone = phoneEditor.getText().toString();
         String password = phoneEditor.getText().toString();
 
-        // Todo: register with phone and password
+        InputStream stream = null;
+        HttpURLConnection connection = null;
+
+        try {
+            URL url = new URI("http://172.20.10.8:8080/").toURL();
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            stream = connection.getInputStream();
+            if (stream != null) {
+                String result = readStream(stream, 500);
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                stream.close();
+            }
+            connection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Exception: " + e.getLocalizedMessage(),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public String readStream(InputStream stream, int maxReadSize) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] rawBuffer = new char[maxReadSize];
+        int readSize;
+        StringBuffer buffer = new StringBuffer();
+        while (((readSize = reader.read(rawBuffer)) != -1) && maxReadSize > 0) {
+            if (readSize > maxReadSize) {
+                readSize = maxReadSize;
+            }
+            buffer.append(rawBuffer, 0, readSize);
+            maxReadSize -= readSize;
+        }
+        return buffer.toString();
     }
 
 }
